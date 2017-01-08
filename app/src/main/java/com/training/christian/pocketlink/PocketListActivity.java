acb5b04040a41d89450dba78d16b5145bacca22c
Chrisian Goltz <christiangoltz@hotmail.com>
2017-01-08 15:38:37 +0100

    Initial commit

---
+++ app/src/main/java/com/training/christian/pocketlink/PocketListActivity.java
@@ -0,0 +1,27 @@
+package com.training.christian.pocketlink;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PocketListActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_pocket_list)
+    RecyclerView mList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pocket_list);
+        ButterKnife.bind(this);
+
+        //Wyswietlanie elementow w pionie, jeden za drugim
+        mList.setLayoutManager(new LinearLayoutManager(this));
+
+
+    }
+}
